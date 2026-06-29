3e6e179273f0e3d7f3157591709a3dd0b7b4ac26
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-06-29 10:34:55 +0530

    new frontend

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,51 +1,51 @@
 
 export default {
-  create_user: "https://manit-clean-track-1.onrender.com/api/user/create-user",
-  send_otp: "https://manit-clean-track-1.onrender.com/api/auth/send-otp",
-  verify_otp: "https://manit-clean-track-1.onrender.com/api/auth/verify-email-otp",
-  get_All_User: "https://manit-clean-track-1.onrender.com/api/user/all-users",
-  create_Emg_Req: "https://manit-clean-track-1.onrender.com/api/emergency/create",
+  create_user: "https://vercel-backend-vboc.vercel.app//api/user/create-user",
+  send_otp: "https://vercel-backend-vboc.vercel.app//api/auth/send-otp",
+  verify_otp: "https://vercel-backend-vboc.vercel.app//api/auth/verify-email-otp",
+  get_All_User: "https://vercel-backend-vboc.vercel.app//api/user/all-users",
+  create_Emg_Req: "https://vercel-backend-vboc.vercel.app//api/emergency/create",
 
 
 
 get_All_Emg_Req:
-"https://manit-clean-track-1.onrender.com/api/emergency/getAllEmgReq",
+"https://vercel-backend-vboc.vercel.app//api/emergency/getAllEmgReq",
 
 update_Emg_Order:
-"https://manit-clean-track-1.onrender.com/api/emergency/schedule-req",
+"https://vercel-backend-vboc.vercel.app//api/emergency/schedule-req",
 
 get_All_Locations:
-"https://manit-clean-track-1.onrender.com/api/location/all",
+"https://vercel-backend-vboc.vercel.app//api/location/all",
 
 get_My_Emg_Req:
-"https://manit-clean-track-1.onrender.com/api/emergency/my-requests",
+"https://vercel-backend-vboc.vercel.app//api/emergency/my-requests",
 
 
 get_Todays_Task:
-"https://manit-clean-track-1.onrender.com/api/driver/today-task",
+"https://vercel-backend-vboc.vercel.app//api/driver/today-task",
 
-arrived_At_Task:"https://manit-clean-track-1.onrender.com/api/driver/arrived",
-completed_Task:"https://manit-clean-track-1.onrender.com/api/driver/completed",
+arrived_At_Task:"https://vercel-backend-vboc.vercel.app//api/driver/arrived",
+completed_Task:"https://vercel-backend-vboc.vercel.app//api/driver/completed",
 
-// completed_Task: "https://manit-clean-track-1.onrender.com/api/driver/completed",
+// completed_Task: "https://vercel-backend-vboc.vercel.app//api/driver/completed",
 
 
-create_Location:"https://manit-clean-track-1.onrender.com/api/location/create",
+create_Location:"https://vercel-backend-vboc.vercel.app//api/location/create",
 
 
 assign_Zone:
-"https://manit-clean-track-1.onrender.com/api/supervisor/assign-zone",
+"https://vercel-backend-vboc.vercel.app//api/supervisor/assign-zone",
 
 get_Supervisor_Locations:
-"https://manit-clean-track-1.onrender.com/api/supervisor/getsupervisor-location",
+"https://vercel-backend-vboc.vercel.app//api/supervisor/getsupervisor-location",
 
 get_Locations_By_Zone:
-"https://manit-clean-track-1.onrender.com/api/location/get-location-by-zone",
+"https://vercel-backend-vboc.vercel.app//api/location/get-location-by-zone",
 
 
 
 delete_User:
-"https://manit-clean-track-1.onrender.com/api/user/delete-user",
+"https://vercel-backend-vboc.vercel.app//api/user/delete-user",
 };
 
 
