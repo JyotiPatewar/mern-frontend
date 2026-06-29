d800d527c41fa088fd5aa9054050ecd4ae68087f
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-06-29 16:30:01 +0530

    Fix ESLint warnings and update dashboard

--- src/components/LocationCreatedashboard.js
+++ src/components/LocationCreatedashboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import axios from "axios";
 import Api from "../api/Api";
 import { toast } from "react-toastify";
