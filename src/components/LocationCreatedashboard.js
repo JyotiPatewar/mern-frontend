997a744bc5604922960e687b3742d6c930272524
Jyoti Patewar <jyotipatewar2004@gmail.com>
2026-07-07 10:45:58 +0530

    Added location APIs

--- src/components/LocationCreatedashboard.js
+++ src/components/LocationCreatedashboard.js
@@ -396,6 +396,7 @@ const handleSubmit = async (e) => {
     (id ? "Failed to update location" : "Failed to create location")
   );
 };
+}
   return (
     <div className="min-h-screen bg-[#4CBB17]/20">
 
@@ -545,6 +546,5 @@ const handleSubmit = async (e) => {
 
     </div>
   );
-}
 }
 export default LocationCreatedashboard;
